78b135b72e7612774e1e7c061134f37dc8125be7
Bruno Arsene <arsene.bruno@gmail.com>
2023-03-23 19:34:47 -0600

    Add some status messages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,63 @@
 <script lang="ts">
 	let link: string | undefined;
-	let loaded = false;
+	let status: 'idle' | 'loading' | 'loaded' = 'idle';
 
 	const currentTab = { active: true, currentWindow: true };
 
 	async function getUrl() {
-		const [tab] = await chrome.tabs.query(currentTab);
-		const url = tab.url;
+		try {
+			status = 'loading';
 
-		const response = await fetch(`https://archive.is/${url}`);
-		const data = await response.text();
+			const [tab] = await chrome.tabs.query(currentTab);
+			const url = tab.url;
 
-		const container = document.createElement('div');
-		container.innerHTML = data;
+			const response = await fetch(`https://archive.is/${url}`);
+			const data = await response.text();
 
-		link =
-			(
-				container.querySelector('#row0 > div.TEXT-BLOCK > a:nth-child(1)') as
-					| HTMLAnchorElement
-					| undefined
-			)?.href ?? '';
+			const container = document.createElement('div');
+			container.innerHTML = data;
 
-		link && await chrome.tabs.update(tab.id as number, {
-			url: link
-		});
-		loaded = true;
+			link =
+				(
+					container.querySelector('#row0 > div.TEXT-BLOCK > a:nth-child(1)') as
+						| HTMLAnchorElement
+						| undefined
+				)?.href ?? '';
+
+			link &&
+				(await chrome.tabs.update(tab.id as number, {
+					url: link
+				}));
+		} finally {
+			status = 'loaded';
+		}
 	}
 </script>
 
-<button on:click={getUrl}>Go to archived version</button>
-
-{#if loaded && !link}
-	<div>No archive found</div>
+{#if status === 'idle'}
+	<button on:click={getUrl}>Go to archived version</button>
+{:else if status === 'loading'}
+	<p>Looking for an archived version of this page.</p>
+{:else if status === 'loaded' && !link}
+	<p>No archive found</p>
+{:else if status === 'loaded'}
+	<p>
+		Archive found. <br />
+		Redirecting to page...
+	</p>
 {/if}
+
+<style>
+	button {
+		color: #222;
+		display: inline-block;
+		width: 100%;
+		height: 100%;
+		border: none;
+		transition: all 0.15s ease-in;
+	}
+
+	button:hover {
+		background: #ccc;
+	}
+</style>
