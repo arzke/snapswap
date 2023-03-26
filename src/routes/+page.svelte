88cbfcef61ff7938a8c572791991554d97aa1c63
Bruno Arsene <arsene.bruno@gmail.com>
2023-03-26 11:44:29 -0600

    Use spaces instead of tabs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,63 @@
 <script lang="ts">
-	let link: string | undefined;
-	let status: 'idle' | 'loading' | 'loaded' = 'idle';
+  let link: string | undefined;
+  let status: 'idle' | 'loading' | 'loaded' = 'idle';
 
-	const currentTab = { active: true, currentWindow: true };
+  const currentTab = { active: true, currentWindow: true };
 
-	async function getUrl() {
-		try {
-			status = 'loading';
+  async function getUrl() {
+    try {
+      status = 'loading';
 
-			const [tab] = await chrome.tabs.query(currentTab);
-			const url = tab.url;
+      const [tab] = await chrome.tabs.query(currentTab);
+      const url = tab.url;
 
-			const response = await fetch(`https://archive.is/${url}`);
-			const data = await response.text();
+      const response = await fetch(`https://archive.is/${url}`);
+      const data = await response.text();
 
-			const container = document.createElement('div');
-			container.innerHTML = data;
+      const container = document.createElement('div');
+      container.innerHTML = data;
 
-			link =
-				(
-					container.querySelector('#row0 > div.TEXT-BLOCK > a:nth-child(1)') as
-						| HTMLAnchorElement
-						| undefined
-				)?.href ?? '';
+      link =
+        (
+          container.querySelector('#row0 > div.TEXT-BLOCK > a:nth-child(1)') as
+            | HTMLAnchorElement
+            | undefined
+        )?.href ?? '';
 
-			link &&
-				(await chrome.tabs.update(tab.id as number, {
-					url: link
-				}));
-		} finally {
-			status = 'loaded';
-		}
-	}
+      link &&
+        (await chrome.tabs.update(tab.id as number, {
+          url: link
+        }));
+    } finally {
+      status = 'loaded';
+    }
+  }
 </script>
 
 {#if status === 'idle'}
-	<button on:click={getUrl}>Go to archived version</button>
+  <button on:click={getUrl}>Go to archived version</button>
 {:else if status === 'loading'}
-	<p>Looking for an archived version of this page.</p>
+  <p>Looking for an archived version of this page.</p>
 {:else if status === 'loaded' && !link}
-	<p>No archive found</p>
+  <p>No archive found</p>
 {:else if status === 'loaded'}
-	<p>
-		Archive found. <br />
-		Redirecting to page...
-	</p>
+  <p>
+    Archive found. <br />
+    Redirecting to page...
+  </p>
 {/if}
 
 <style>
-	button {
-		color: #222;
-		display: inline-block;
-		width: 100%;
-		height: 100%;
-		border: none;
-		transition: all 0.15s ease-in;
-	}
+  button {
+    color: #222;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    border: none;
+    transition: all 0.15s ease-in;
+  }
 
-	button:hover {
-		background: #ccc;
-	}
+  button:hover {
+    background: #ccc;
+  }
 </style>
